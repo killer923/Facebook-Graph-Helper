224bfb4e8ab11c63168ee3c2483c862f31f2deda
Abhishek Bhatia <abhatia923@gmail.com>
2014-08-20 01:51:14 +0530

    Removing v2.2

--- src/main/java/com/killer923/fb/model/FacebookVersion.java
+++ src/main/java/com/killer923/fb/model/FacebookVersion.java
@@ -5,8 +5,8 @@ public enum FacebookVersion
 	unversionsed("unversioned","http://graph.facebook.com/"),
 	v1("v1","http://graph.facebook.com/v1.0/"),
 	v2_0("v2.0","http://graph.facebook.com/v2.0/"),
-	v2_1("v2.1","http://graph.facebook.com/v2.1/"),
-	v2_2("v2.2","http://graph.facebook.com/v2.2/");
+	v2_1("v2.1","http://graph.facebook.com/v2.1/");
+//	v2_2("v2.2","http://graph.facebook.com/v2.2/");
 	
 	
 	private String version;
